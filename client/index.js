38d38da6db98c8c2ca3bb8c422c60e4ab06376bd
AlexDjangoX <alexmonk17@gmail.com>
2023-06-16 10:34:57 +0200

    solution

---
+++ client/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  const tree = new MerkleTree(niceList);
+  const name = niceList[0];
+
+  const index = niceList.indexOf(name);
+
+  if (index === -1) {
+    console.log('Name not in the list');
+    return;
+  }
+
+  const proof = tree.getProof(niceList.indexOf(name));
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    name,
+    proof,
+  });
+
+  console.log({ gift });
+}
+
+main();
